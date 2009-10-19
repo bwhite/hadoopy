060f43e5df083a9bebfd4611c62fb11adbd80a1b
Brandyn White <brandyn@ubuntu.(none)>
2009-10-19 15:59:47 -0700

    Version bump.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='hadoopy',
-    version='.02',
+    version='.03',
     install_requires=['numpy',],
     zip_safe = True,
     packages = find_packages()
