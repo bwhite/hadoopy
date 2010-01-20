25f10099d391bfe7fbe25cf57bcccd2cc27efc50
Brandyn White <brandyn@router.(none)>
2010-01-19 19:42:35 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='hadoopy',
-    version='.05',
+    version='.06',
     install_requires=['numpy',],
     zip_safe = True,
     packages = find_packages()
