0857c0614008871bbf62ad3f4c6d7a9f16e64b8b
Brandyn White <bwhite@dappervision.com>
2010-03-30 18:30:47 -0400

    Removed old functions

--- hadoopy/__init__.py
+++ hadoopy/__init__.py
@@ -2,7 +2,5 @@ from pickle import pickle
 from pickle import  array_pickle
 import runner
 from run import run
-from run import configure
-from run import close
 from reporter import status
 from reporter import counter
