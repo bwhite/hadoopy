2b6ce59e107759f138953eef376fc6aa9adc583c
Brandyn White <bwhite@dappervision.com>
2010-03-30 18:08:12 -0400

    Changed how run handles configure, close, and classes.  Added missing __init__

--- hadoopy/run.py
+++ hadoopy/run.py
@@ -1,6 +1,6 @@
 import sys
-import itertools
-import operator
+from operator import itemgetter
+from itertools import groupby
 
 
 def _print_out(out, separator):
@@ -16,7 +16,8 @@ def _key_values(separator='\t'):
 
 
 def _groupby_key_values(separator='\t'):
-    return ((x, (z[1] for z in y)) for x, y in itertools.groupby(_key_values(separator), operator.itemgetter(0)))
+    return ((x, (z[1] for z in y))
+            for x, y in groupby(_key_values(separator), itemgetter(0)))
 
 
 def _offset_values():
@@ -27,26 +28,48 @@ def _offset_values():
         line_count += 1
 
 
-def _handle_map(mapper, separator):
-    for key, value in _offset_values():
-        for out in mapper(key, value):
-            if out:
-                _print_out(out, separator)
-    return 0
-
-
-def _handle_reduce(reducer, separator):
-    for key, values in _groupby_key_values(separator):
-        for out in reducer(key, values):
-            if out:
-                _print_out(out, separator)
-    return 0
-
-
 def _handle_final(func, separator):
     for out in func():
         if out:
             _print_out(out, separator)
+    return 0
+
+
+def configure_call_close(attr):
+    def factory(f):
+        def inner(func, separator):
+            try:
+                func.configure()
+            except AttributeError:
+                pass
+            try:
+                return f(getattr(func, attr), separator)
+            except AttributeError:
+                return f(func, separator)
+            finally:
+                try:
+                    _handle_final(func.close, separator)
+                except AttributeError:
+                    pass
+        return inner
+    return factory
+
+
+@configure_and_close('map')
+def _handle_map(func, separator):
+    for key, value in _offset_values():
+        for out in func(key, value):
+            if out:
+                _print_out(out, separator)
+    return 0
+
+
+@configure_and_close('reduce')
+def _handle_reduce(func, separator):
+    for key, values in _groupby_key_values(separator):
+        for out in func(key, values):
+            if out:
+                _print_out(out, separator)
     return 0
 
 
@@ -60,34 +83,4 @@ def run(mapper=None, reducer=None, combiner=None, separator='\t'):
         return _handle_reduce(reducer, separator)
     elif method == 'combine':
         return _handle_reduce(combiner, separator)
-    return 1
-
-
-def configure(mapper=None, reducer=None, combiner=None):
-    """Used to call the proper function depending on arguments"""
-    # TODO Should throw exception
-    if len(sys.argv) < 2:
-        return
-    method = sys.argv[1]
-    if method == 'map':
-        return mapper()
-    elif method == 'reduce':
-        return reducer()
-    elif method == 'combine':
-        return combiner()
-
-def close(mapper=None, reducer=None, combiner=None, separator='\t'):
-    """Used to call provided functions when there is no more input.
-    This is used for patterns such as in-map-combiner where you need
-    a stable destructor to call at the end to flush the buffered data.
-    """
-    if len(sys.argv) < 2:
-        return 1
-    method = sys.argv[1]
-    if method == 'map':
-        return _handle_final(mapper, separator)
-    elif method == 'reduce':
-        return _handle_final(reducer, separator)
-    elif method == 'combine':
-        return _handle_final(combiner, separator)
     return 1
