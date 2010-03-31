9782ea9f0f46d7b9a08c416244e33357f38ea9ce
Brandyn White <bwhite@dappervision.com>
2010-03-31 14:20:14 -0400

    Now typedbytes support is all or nothing, it simplifies the code and I don't see any reason to do it in parts.  The main reason to keep text around is for older versions of hadoop or unit testing.

--- hadoopy/run.py
+++ hadoopy/run.py
@@ -1,42 +1,78 @@
 import sys
+import os
 from operator import itemgetter
 from itertools import groupby
 import typedbytes
-import StringIO
 
 
-def _key_values():
-    for x in typedbytes.PairedInput(sys.stdin):
-        yield x
+def _key_values_text(sep='\t'):
+    for line in sys.stdin:
+        yield line.rstrip().split(sep, 1)
 
 
-def _groupby_key_values():
+def _key_values_tb():
+    return typedbytes.PairedInput(sys.stdin).reads()
+
+
+def _groupby_key_values(kv):
     return ((x, (z[1] for z in y))
-            for x, y in groupby(_key_values(), itemgetter(0)))    
+            for x, y in groupby(kv, itemgetter(0)))
 
 
-def _offset_values():
+def _offset_values_text():
     line_count = 0
     for line in sys.stdin:
-        line = line[:-1]
-        yield line_count, line
-        line_count += 1
+        yield line_count, line[:-1]
+        line_count += len(line)
+
+
+def _is_io_typedbytes():
+    # Only all or nothing typedbytes is supported, just check stream_map_input
+    try:
+        return os.environ['stream_map_input'] == 'typedbytes'
+    except KeyError:
+        return False
+
+
+def _read_in_map():
+    if _is_io_typedbytes():
+        return _key_values_tb()
+    return _offset_values_text()
+
+
+def _read_in_reduce():
+    if _is_io_typedbytes():
+        return _key_values_tb()
+    return _key_values_text()
+
+
+def _print_out_text(iter, sep='\t'):
+    for out in iter:
+        if isinstance(out, tuple):
+            print(sep.join(str(x) for x in out))
+        else:
+            print(str(out))
+
+
+def _print_out_tb(iter):
+    typedbytes.PairedOutput(sys.stdout).writes(iter)
 
 
 def _print_out(iter):
-    out = StringIO.StringIO('w')
-    typedbytes.PairedOutput(out).writes(iter)
-    out.seek(0)
-    print(out.read())
+    if _is_io_typedbytes():
+        _print_out_tb(iter)
+    else:
+        _print_out_text(iter)
 
-def _final(func, sep):
+
+def _final(func):
     _print_out(func())
     return 0
 
 
 def _configure_call_close(attr):
     def factory(f):
-        def inner(func, sep):
+        def inner(func):
             if func == None:
                 return 1
             if isinstance(func, type):
@@ -47,14 +83,14 @@ def _configure_call_close(attr):
                 pass
             try:
                 try:
-                    return f(getattr(func, attr), sep)
+                    return f(getattr(func, attr))
                 except AttributeError:
-                    return f(func, sep)
-            except ValueError: # func not generator, its ok
+                    return f(func)
+            except ValueError:  # func not generator, its ok
                 return 0
             finally:
                 try:
-                    _final(func.close, sep)
+                    _final(func.close)
                 except AttributeError:
                     pass
         return inner
@@ -62,27 +98,28 @@ def _configure_call_close(attr):
 
 
 @_configure_call_close('map')
-def _map(func, sep):
-    for key, value in _offset_values():
+def _map(func):
+    for key, value in _read_in_map():
         _print_out(func(key, value))
     return 0
 
 
 @_configure_call_close('reduce')
-def _reduce(func, sep):
-    for key, values in _groupby_key_values():
+def _reduce(func):
+    for key, values in _groupby_key_values(_read_in_reduce()):
         _print_out(func(key, values))
     return 0
 
 
-def run(mapper=None, reducer=None, combiner=None, sep='\t'):
-    funcs = {'map': lambda: _map(mapper, sep),
-             'reduce': lambda: _reduce(reducer, sep),
-             'combine': lambda: _reduce(combiner, sep)}
+def run(mapper=None, reducer=None, combiner=None):
+    funcs = {'map': lambda: _map(mapper),
+             'reduce': lambda: _reduce(reducer),
+             'combine': lambda: _reduce(combiner)}
     try:
         return funcs[sys.argv[1]]()
     except (IndexError, KeyError):
         return 1
+
 
 def print_doc_quit(doc):
     print(doc)
