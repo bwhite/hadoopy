9e4fc18cb41bb6b7135b5e41ffde938019d8423a
Brandyn White <bwhite@dappervision.com>
2010-04-01 20:57:25 -0400

    Added HDFS listing and catting

--- hadoopy/__init__.py
+++ hadoopy/__init__.py
@@ -1,7 +1,7 @@
-import pickle
-import array_pickle
 import runner
 from hdfs import hdfs_cat_tb
+from hdfs import hdfs_ls
+from freeze import freeze
 from run import run
 from run import print_doc_quit
 from reporter import status
--- hadoopy/hdfs.py
+++ hadoopy/hdfs.py
@@ -1,17 +1,46 @@
 import subprocess
 import typedbytes
 import tempfile
+import re
 
 from hadoopy.runner import _find_hstreaming
 
 
+def hdfs_ls(path):
+    """List files on HDFS.
+
+    Args:
+      path: A string (potentially with wildcards).
+
+    Returns:
+      A list of strings representing HDFS paths.
+
+    Raises:
+      IOError: An error occurred listing the directory (e.g., not available).
+    """
+    out, err = subprocess.Popen(['hadoop', 'fs', '-ls', path],
+                                stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+    if err:
+        raise IOError, err
+    found_line = lambda x: re.search('Found [0-9]+ items$', x)
+    out = [x.split(' ')[-1] for x in out.split('\n') if x and not found_line(x)]
+    return out
+
 def hdfs_cat_tb(path):
-    """Used to read files off of HDFS that have been stored as
-    typedbytes in a sequence file and optionally compressed.
-    Wildcards are acceptable. The output format is an iterator of
-    key, value pairs."""
+    """Read typedbytes sequence files on HDFS (with optional compression).
+
+    Args:
+      path: A string (potentially with wildcards).
+
+    Returns:
+      An iterator of key, value pairs.
+
+    Raises:
+      IOError: An error occurred listing the directory (e.g., not available).
+    """
     fp = tempfile.TemporaryFile()
-    subprocess.Popen(['hadoop', 'jar', _find_hstreaming(), 'dumptb', path],
-                     stdout=fp).wait()
+    for x in hdfs_ls(path):
+        subprocess.Popen(['hadoop', 'jar', _find_hstreaming(), 'dumptb', x],
+                         stdout=fp, stderr=subprocess.PIPE).wait()
     fp.seek(0)
     return typedbytes.PairedInput(fp).reads()
