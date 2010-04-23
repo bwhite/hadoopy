4546d888c588afa90551c3db74934d24e8cccb1a
Brandyn <bwhite@cs.umd.edu>
2010-04-23 14:34:35 -0400

    Cleaned up test code

--- hadoopy/test.py
+++ hadoopy/test.py
@@ -3,23 +3,15 @@ from hadoopy.main import _configure_call_close
 
 class Test(unittest.TestCase):
     
-    def __init__(self, name='runTest'):
-        super(Test, self).__init__(name)
+    def __init__(self, *args, **kw):
+        super(Test, self).__init__(*args, **kw)
+        self.call_map = self._call('map')
+        self.call_reduce = self._call('reduce')
 
-    @staticmethod
-    @_configure_call_close('map')
-    def _call_map(func, test_input):
-        return [func(key, value) for key, value in test_input]
-
-    @staticmethod
-    @_configure_call_close('reduce')
-    def _call_reduce(func, test_input):
-        return [func(key, value) for key, value in test_input]
-
-    def call_map(self, func, test_input):
-        self._call_map(func, test_input)
-
-    def call_reduce(self, func, test_input):
-        self._call_reduce(func, test_input)
-
-        
+    def _call(self, attr):
+        def call_func(func, test_input):
+            @_configure_call_close(attr)
+            def _in(func):
+                return sum([list(func(*x)) for x in test_input], [])
+            return _in(func)
+        return call_func
