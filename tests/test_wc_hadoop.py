67fdbfe1a18f45c3737fc65e42d0b75b495accd2
brandyn <brandyn@brandynd2.cs.umd.edu>
2010-06-01 03:25:46 -0400

    Test headers

--- tests/test_wc_hadoop.py
+++ tests/test_wc_hadoop.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python
+# (C) Copyright 2010 Brandyn A. White
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+__author__ =  'Brandyn A. White <bwhite@cs.umd.edu>'
+__licence__ = 'GPL V3'
+
 import subprocess
 import time
 import unittest
