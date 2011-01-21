ab68086556b5949229fb04b74264e6b8b65af23a
Brandyn White <bwhite@cs.umd.edu>
2011-01-21 12:43:22 -0500

    Cleaned up speed test, updated TypedBytes examples with Klaas's suggestions,  this requires my patched cTypedBytes

--- play/speed_tbc.py
+++ play/speed_tbc.py
@@ -4,5 +4,4 @@ import sys
 
 b = typedbytes.PairedInput(sys.stdin)
 c = typedbytes.PairedOutput(sys.stdout)
-for x in b:
-    c.write(x)
+c.writes(b)
--- play/speedtest.py
+++ play/speedtest.py
@@ -12,6 +12,14 @@ def parse_tb(val):
         yield x
 
 
+def time_script(script_name, data):
+    st = time.time()
+    p = subprocess.Popen(('python %s' % script_name).split(),
+                         stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    o = p.communicate(data)[0]
+    return o, time.time() - st
+
+
 def main():
     out = []
     print('+-----------------+---------+---------+---------+---------+---------+')
@@ -20,34 +28,12 @@ def main():
     for fn in sorted(glob.glob('*.tb')):
         with open(fn) as fp:
             data = fp.read()
-        st = time.time()
-        p = subprocess.Popen('python speed_hadoopy.py'.split(),
-                             stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-        o0 = p.communicate(data)[0]
-        t0 = time.time() - st
-        st = time.time()
-        p = subprocess.Popen('python speed_hadoopyfp.py'.split(),
-                             stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-        o1 = p.communicate(data)[0]
-        t1 = time.time() - st
-        st = time.time()
-        p = subprocess.Popen('python speed_tb.py'.split(),
-                             stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-        o2 = p.communicate(data)[0]
-        t2 = time.time() - st
-        p = subprocess.Popen('python speed_tbc.py'.split(),
-                             stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-        o3 = p.communicate(data)[0]
-        t3 = time.time() - st
-        out.append((fn, t0, t1, t2, t3, min(t2, t3) / t1))
-        #print('%s:\tHadoopy[%2.6f] Hadoopyfp[%2.6f] Typedbytes[%2.6f] cTypedbytes[%2.6f]:  min(TypedBytes, cTypedBytes) / Hadoopyfp = %f' % (fn, t0, t1, t2, t3, min(t2, t3) / t1))
+        o0, t0 = time_script('speed_hadoopy.py', data)
+        o1, t1 = time_script('speed_hadoopyfp.py', data)
+        o2, t2 = time_script('speed_tb.py', data)
+        o3, t3 = time_script('speed_tbc.py', data)
+        out.append((fn, t0, t1, t2, t3, min([t2, t3]) / t1))
         assert(o0 == o1 == o2 == o3)
-        #for x, y, z in itertools.izip(parse_tb(o0), parse_tb(o1), parse_tb(o2)):
-        #    try:
-        #        assert(x == y == z)
-        #    except AssertionError, e:
-        #        print('x:%r\ny:%r\nz:%r' % (x, y, z))
-        #        raise e
     out.sort(lambda x, y: cmp(x[-1], y[-1]), reverse=True)
     for x in out:
         print('|%17s|%9.6f|%9.6f|%9.6f|%9.6f|%9.6f|' % x)
