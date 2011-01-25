f9b5e0e82817a563aa64e46d2b9e18d5c54cb547
Brandyn White <bwhite@cs.umd.edu>
2011-01-24 19:49:03 -0500

    Made setup.py a bit nicer

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 from distutils.core import setup
 from distutils.extension import Extension
+
+# TODO: Only use Cython if it is available, else just use the .c
 from Cython.Distutils import build_ext
 
 ext_modules = [Extension("_main", ["hadoopy/_main.pyx"]),
@@ -9,4 +11,8 @@ setup(name='hadoopy',
       cmdclass={'build_ext': build_ext},
       version='.2',
       packages=['hadoopy'],
+      author='Brandyn A. White',
+      author_email='bwhite@dappervision.com',
+      license='GPL',
+      url='https://github.com/bwhite/hadoopy',
       ext_modules=ext_modules)
