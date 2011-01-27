8b65e3e298a4ea8b6f1a513a4a90b4deccff1718
Brandyn White <bwhite@cs.umd.edu>
2011-01-27 18:06:25 -0500

    Simplified byteconversion even more, added big endian flag to setup.py

--- hadoopy/byteconversion.h
+++ hadoopy/byteconversion.h
@@ -9,20 +9,20 @@
 #define _htobe64(x) (htobe64(x))
 #else
 #include <stdint.h>
+#include <arpa/inet.h>
 #define _be32toh(x) (ntohl(x))
 #define _htobe32(x) (htonl(x))
 #ifdef BYTECONVERSION_ISBIGENDIAN
 #define _be64toh(x) (x)
 #define _htobe64(x) (x)
 #else
-#include <arpa/inet.h>
-inline uint64_t _be64toh(uint64_t val) {
+inline uint64_t _byteswap64bit(uint64_t val) {
     return ((uint64_t)ntohl(val >> 32)) | ((uint64_t)ntohl(val) << 32);
 }
-inline uint64_t _htobe64(uint64_t val) {
-    return ((uint64_t)htonl(val >> 32)) | ((uint64_t)htonl(val) << 32);
-}
-#endif
-#endif
+#define _be64toh(x) (_byteswap64bit(x))
+#define _htobe64(x) (_byteswap64bit(x))
 
-#endif
+#endif /* BYTECONVERSION_ISBIGENDIAN */
+#endif /* BYTECONVERSION_HASENDIAN_H */
+
+#endif /* BYTECONVERSION_H */
--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
+import sys
 from distutils.core import setup
 from distutils.extension import Extension
 
 # TODO: Only use Cython if it is available, else just use the .c
 from Cython.Distutils import build_ext
 
+tb_extra_args = []
+if sys.byteorder != 'little':
+    tb_extra_args.append('-D BYTECONVERSION_ISBIGENDIAN')
+
 ext_modules = [Extension("_main", ["hadoopy/_main.pyx"]),
-               Extension("_typedbytes", ["hadoopy/_typedbytes.pyx"])]
+               Extension("_typedbytes", ["hadoopy/_typedbytes.pyx"],
+                         extra_compile_args=tb_extra_args)]
 setup(name='hadoopy',
       cmdclass={'build_ext': build_ext},
       version='.2',
