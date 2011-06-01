d0d8d3999eeb19ec0d8d5d0da408ed18b54fadb4
Brandyn A. White <bwhite@dappervision.com>
2011-06-01 01:34:32 -0400

    Cleaned up test code Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- tests/test_writetb.py
+++ tests/test_writetb.py
@@ -4,36 +4,7 @@ except ImportError:
     import unittest
 import hadoopy
 import time
-
-# Cheat Sheet (method/test) <http://docs.python.org/library/unittest.html>
-#
-# assertEqual(a, b)       a == b   
-# assertNotEqual(a, b)    a != b    
-# assertTrue(x)     bool(x) is True  
-# assertFalse(x)    bool(x) is False  
-# assertRaises(exc, fun, *args, **kwds) fun(*args, **kwds) raises exc
-# assertAlmostEqual(a, b)  round(a-b, 7) == 0         
-# assertNotAlmostEqual(a, b)          round(a-b, 7) != 0
-# 
-# Python 2.7+ (or using unittest2)
-#
-# assertIs(a, b)  a is b
-# assertIsNot(a, b) a is not b
-# assertIsNone(x)   x is None
-# assertIsNotNone(x)  x is not None
-# assertIn(a, b)      a in b
-# assertNotIn(a, b)   a not in b
-# assertIsInstance(a, b)    isinstance(a, b)
-# assertNotIsInstance(a, b) not isinstance(a, b)
-# assertRaisesRegexp(exc, re, fun, *args, **kwds) fun(*args, **kwds) raises exc and the message matches re
-# assertGreater(a, b)       a > b
-# assertGreaterEqual(a, b)  a >= b
-# assertLess(a, b)      a < b
-# assertLessEqual(a, b) a <= b
-# assertRegexpMatches(s, re) regex.search(s)
-# assertNotRegexpMatches(s, re)  not regex.search(s)
-# assertItemsEqual(a, b)    sorted(a) == sorted(b) and works with unhashable objs
-# assertDictContainsSubset(a, b)      all the key/value pairs in a exist in b
+import numpy as np
 
 class Test(unittest.TestCase):
 
@@ -45,8 +16,9 @@ class Test(unittest.TestCase):
 
     def test_name(self):
         fn = '_tests/writetb-%f' % time.time()
-        hadoopy.writetb(fn, [(x, x) for x in range(100000)])
-        print(len(list(hadoopy.readtb(fn))))
+        data = [('1', 1), (1.3, np.array([1,2,3])), (True, {'1': 3})]
+        hadoopy.writetb(fn, data)
+        np.testing.assert_equal(list(hadoopy.readtb(fn)), data)
 
 if __name__ == '__main__':
     unittest.main()
