4d8761ec6a708b91f73e34a0640f51ca19f2fe1f
Brandyn A. White <bwhite@dappervision.com>
2011-06-04 22:30:59 -0400

    In progress on launch_local, basic operation is working Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- hadoopy/_runner.py
+++ hadoopy/_runner.py
@@ -17,7 +17,6 @@
 __author__ = 'Brandyn A. White <bwhite@cs.umd.edu>'
 __license__ = 'GPL V3'
 
-import sys
 
 import subprocess
 import os
@@ -51,8 +50,8 @@ def launch(in_name, out_name, script_path, mapper=True, reducer=True,
            cmdenvs=(), copy_script=True, hstreaming=None, name=None,
            use_typedbytes=True, use_seqoutput=True, use_autoinput=True,
            pretend=False, add_python=True, config=None, pipe=True,
-           python_cmd="python", num_mappers=None, num_reducers=None, 
-           script_dir='',**kw):
+           python_cmd="python", num_mappers=None, num_reducers=None,
+           script_dir='', **kw):
     """Run Hadoop given the parameters
 
     Args:
@@ -82,7 +81,7 @@ def launch(in_name, out_name, script_path, mapper=True, reducer=True,
         pipe: If true then call user code through a pipe to isolate it
             and stop bugs when printing to stdout.  See project docs.
         python_cmd: The python command to use. The default is "python".
-            Can be used to override the system default python, e.g. 
+            Can be used to override the system default python, e.g.
             python_cmd = "python2.6"
         num_mappers: The number of mappers to use, i.e. the
             argument given to 'numMapTasks'. If None, then
@@ -195,7 +194,7 @@ def launch(in_name, out_name, script_path, mapper=True, reducer=True,
     if not pretend:
         print('/\\%s%s Output%s/\\' % ('-' * 10, 'Hadoop', '-' * 10))
         print('hadoopy: Running[%s]' % (' '.join(cmd)))
-        subprocess.check_call(' '.join(cmd),shell=True)
+        subprocess.check_call(' '.join(cmd), shell=True)
         print('\\/%s%s Output%s\\/' % ('-' * 10, 'Hadoop', '-' * 10))
     return ' '.join(cmd)
 
@@ -229,7 +228,20 @@ def launch_frozen(in_name, out_name, script_path, temp_path='_hadoopy_temp',
         use_autoinput: If True (default), sets the input format to auto.
         pretend: If true, only build the command and return.
         add_python: If true, use 'python script_name.py'
-        verbose: If true, output to stdout all command results.
+        config: If a string, set the hadoop config path
+        pipe: If true then call user code through a pipe to isolate it
+            and stop bugs when printing to stdout.  See project docs.
+        python_cmd: The python command to use. The default is "python".
+            Can be used to override the system default python, e.g.
+            python_cmd = "python2.6"
+        num_mappers: The number of mappers to use, i.e. the
+            argument given to 'numMapTasks'. If None, then
+            do not specify this argument to hadoop streaming.
+        num_reducers: The number of reducers to use, i.e. the
+            argument given to 'numReduceTasks'. If None, then
+            do not specify this argument to hadoop streaming.
+        script_dir: Where the script is relative to working dir, will be
+            prefixed to script_path with a / (default '' is current dir)
 
     Returns:
         The hadoop command called.
@@ -241,11 +253,7 @@ def launch_frozen(in_name, out_name, script_path, temp_path='_hadoopy_temp',
     frozen_tar_path = temp_path + '/%f/_frozen.tar' % time.time()
     freeze_fp = tempfile.NamedTemporaryFile(suffix='.tar')
     hadoopy._freeze.freeze_to_tar(os.path.abspath(script_path), freeze_fp.name)
-    # TODO: Put this file on hdfs
-    subprocess.call(('hadoop fs -put %s %s' % (freeze_fp.name,
-                                               frozen_tar_path)).split())
-    #hadoopy.put(freeze_fp.name, frozen_tar_path)
-    # Remove extension
+    hadoopy.put(freeze_fp.name, frozen_tar_path)
     if script_path.endswith('.py'):
         script_path = script_path[:-3]
     try:
@@ -255,13 +263,87 @@ def launch_frozen(in_name, out_name, script_path, temp_path='_hadoopy_temp',
     else:
         if isinstance(jobconfs, str):
             jobconfs = [jobconfs]
-    
     jobconfs.append('"mapred.cache.archives=%s#_frozen"' % frozen_tar_path)
     jobconfs.append('"mapreduce.job.cache.archives=%s#_frozen"' % frozen_tar_path)
-    
     kw['copy_script'] = False
     kw['add_python'] = False
     kw['jobconfs'] = jobconfs
     launch_cmd = launch(in_name, out_name, script_path,
                         script_dir='_frozen', **kw)
     return launch_cmd
+
+
+def launch_local(in_name, out_name, script_path, max_input=-1,
+                 **kw):
+    """A simple local emulation of hadoop
+
+    This doesn't run hadoop and it doesn't support many advanced features, it
+    is intended for simple debugging.  The input/output is read from HDFS and
+    they must be TypedBytes in SequenceFiles.  The output is stored on HDFS.
+    This allows for small tasks to be run locally (primarily while debugging).
+
+    Support
+    - Environmental variables (TODO)
+    - Map-only tasks (TODO)
+    - Combiner (TODO)
+    - Files (TODO)
+    - Display of stdout/stderr (TODO)
+
+    Args:
+        in_name: Input path (string or list)
+        out_name: Output path
+        script_path: Path to the script (e.g., script.py)
+        max_input: Maximum number of Mapper inputs, if < 0 (default) then
+            unlimited.
+        mapper: If True, the mapper is "script.py map".
+            If string, the mapper is the value
+        reducer: If True (default), the reducer is "script.py reduce".
+            If string, the reducer is the value
+        combiner: If True, the combiner is "script.py combine" (default False).
+            If string, the combiner is the value
+        files: Extra files (other than the script) (string or list).
+            NOTE: Hadoop copies the files into working directory
+        jobconfs: Extra jobconf parameters (string or list)
+        cmdenvs: Extra cmdenv parameters (string or list)
+        pipe: If true then call user code through a pipe to isolate it
+            and stop bugs when printing to stdout.  See project docs.
+
+    Returns:
+        The hadoop command called.
+
+    Raises:
+        subprocess.CalledProcessError: Hadoop or Cxfreeze error.
+        OSError: Hadoop streaming or Cxfreeze not found.
+    """
+    # Run mapper
+    map_in_r_fd, map_in_w_fd = os.pipe()
+    map_out_r_fd, map_out_w_fd = os.pipe()
+    map_cmd = ('./%s map' % script_path).split()
+    env = dict(os.environ)
+    env['stream_map_input'] = 'typedbytes'
+    a = os.fdopen(map_in_r_fd, 'r')
+    b = os.fdopen(map_out_w_fd, 'w')
+    p = subprocess.Popen(map_cmd,
+                         stdin=a,
+                         stdout=b,
+                         close_fds=True,
+                         env=env)
+    a.close()
+    b.close()
+    with hadoopy.TypedBytesFile(read_fd=map_out_r_fd) as tbfp_r:
+        with hadoopy.TypedBytesFile(write_fd=map_in_w_fd) as tbfp_w:
+            for x in range(100):
+                tbfp_w.write((x, x))
+        for x in tbfp_r:
+            print('From[%s]' % str(x))
+    p.wait()
+    #with :
+    #    for num, kv in enumerate(hadoopy.readtb(in_name)):
+    #        if max_input >= 0 and max_input <= num:
+    #            break
+    #        print(num)
+    #        print(kv[0])
+    #        tbfp_w.write(kv)
+    #print('waiting')
+    #p.wait()
+    return
