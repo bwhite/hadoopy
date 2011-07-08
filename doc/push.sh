d602f152ffd577983845d33642cbe36fb4529213
Brandyn A. White <bwhite@dappervision.com>
2011-07-08 08:36:03 -0400

    Updated Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- doc/push.sh
+++ doc/push.sh
@@ -1,10 +1,1 @@
-make html
-git checkout gh-pages
-pushd ..
-rm -r _*
-mv ./doc/html/* .
-git add -u
-git commit -m "Bump"
-git push
-popd
-git checkout master
+make html && git checkout gh-pages && pushd .. &&rm -r _* && mv ./doc/html/* . && git add -u && git commit -m "Bump" && git push && popd && git checkout master
