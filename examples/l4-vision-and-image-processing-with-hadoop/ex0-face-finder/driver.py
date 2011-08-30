80c73a8b516f8383dd59847c9c8639ccb56badda
Brandyn A. White <bwhite@dappervision.com>
2011-08-30 14:27:08 -0400

    Added face finder example Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

---
+++ examples/l4-vision-and-image-processing-with-hadoop/ex0-face-finder/driver.py
@@ -0,0 +1,39 @@
+import hadoopy
+import gzip
+import subprocess
+import time
+import os
+import cv2
+import numpy as np
+import cStringIO as StringIO
+import Image
+
+data_path = '../../data/'
+
+
+def _run_face(fn):
+    cur_time = time.time()
+    hdfs_base_path = 'hadoopy-test-data/%f/' % cur_time
+    print('Storing HDFS temp files and output in [%s]' % hdfs_base_path)
+    in_path = hdfs_base_path + os.path.basename(fn)
+    out_path = hdfs_base_path + 'out-' + os.path.basename(fn)
+    cmd = 'hadoop fs -put %s %s' % (fn, in_path)
+    subprocess.check_call(cmd.split())
+    hadoopy.launch_frozen(in_path, out_path, 'face_finder.py', reducer=False, files=[data_path + 'haarcascade_frontalface_default.xml'])
+    local_out = 'out-%f' % cur_time
+    try:
+        os.makedirs(local_out)
+    except OSError:
+        pass
+    print('Storing local output in [%s]' % local_out)
+    for num, (image_name, (image_data, faces)) in enumerate(hadoopy.readtb(out_path)):
+        image = np.asarray(Image.open(StringIO.StringIO(image_data)))
+        for (x, y, w, h), n in faces:
+            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 3)
+        cv2.imwrite('%s/img%.8d.jpg' % (local_out, num), image[:, :, ::-1].copy())
+
+
+with open(data_path + 'haarcascade_frontalface_default.xml', 'w') as fp:
+    o = gzip.GzipFile(data_path + 'haarcascade_frontalface_default.xml.gz').read()
+    fp.write(o)
+_run_face(data_path + '/face_finder-input-voctrainpart.tb')
---
+++ examples/l4-vision-and-image-processing-with-hadoop/ex0-face-finder/face_finder.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# (C) Copyright 2011 Brandyn A. White
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Hadoopy Face Finding Demo"""
+
+__author__ = 'Brandyn A. White <bwhite@cs.umd.edu>'
+__license__ = 'GPL V3'
+
+import hadoopy
+import Image
+import imfeat
+import cStringIO as StringIO
+import os
+import cv
+
+
+class Mapper(object):
+
+    def __init__(self):
+        path = 'haarcascade_frontalface_default.xml'
+        if os.path.exists(path):
+            self._cascade = cv.Load(path)
+        else:
+            path = 'fixtures/haarcascade_frontalface_default.xml'
+            if os.path.exists(path):
+                self._cascade = cv.Load(path)
+            else:
+                raise ValueError("Can't find .xml file!")
+
+    def _detect_faces(self, img):
+        min_size = (20, 20)
+        image_scale = 2
+        haar_scale = 1.2
+        min_neighbors = 2
+        haar_flags = 0
+        if img.nChannels == 3:
+            gray = cv.CreateImage((img.width, img.height), 8, 1)
+            cv.CvtColor(img, gray, cv.CV_BGR2GRAY)
+        else:
+            gray = img
+        small_img = cv.CreateImage((cv.Round(img.width / image_scale),
+                                    cv.Round(img.height / image_scale)), 8, 1)
+        cv.Resize(gray, small_img, cv.CV_INTER_LINEAR)
+        cv.EqualizeHist(small_img, small_img)
+        faces = cv.HaarDetectObjects(small_img, self._cascade,
+                                     cv.CreateMemStorage(0),
+                                     haar_scale, min_neighbors, haar_flags,
+                                     min_size)
+        return [((x * image_scale, y * image_scale,
+                  w * image_scale, h * image_scale), n)
+                for (x, y, w, h), n in faces]
+
+    def _load_cv_image(self, value):
+        return imfeat.convert_image(Image.open(StringIO.StringIO(value)),
+                                    [('opencv', 'rgb', 8)])
+
+    def map(self, key, value):
+        """
+        Args:
+            key: Image name
+            value: Image as jpeg byte data
+
+        Yields:
+            A tuple in the form of (key, value)
+            key: Image name
+            value: (image, faces) where image is the input value and faces is
+                a list of ((x, y, w, h), n)
+        """
+        try:
+            image = self._load_cv_image(value)
+        except:
+            hadoopy.counter('DATA_ERRORS', 'ImageLoadError')
+            return
+        faces = self._detect_faces(image)
+        if faces:
+            yield key, (value, faces)
+
+
+if __name__ == "__main__":
+    hadoopy.run(Mapper, doc=__doc__)
