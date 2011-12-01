aaed2e46b8b714ffe55f104efbfee02eda5ae363
Brandyn A. White <bwhite@dappervision.com>
2011-12-01 18:22:07 -0500

    Updated pusher Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- doc/push.sh
+++ doc/push.sh
@@ -1,1 +1,37 @@
-rm -r html && make html && git checkout gh-pages && pushd .. &&rm -r _* && mv ./doc/html/* . && git add -u && git commit -m "Bump" && git push && popd && git checkout master
+set -e
+# Make fresh clone
+git clone ../ temp_clone
+pushd temp_clone/doc/
+
+# Build new docs
+make html
+
+# Copy relevant files to doc_temp
+cd ..
+mv doc/html doc_temp
+
+# Remove previous files and branch
+git checkout --orphan gh-pages
+git rm -rf .
+
+# Ensure that .nojekyll is set for github to ignore _'d dirs
+touch .nojekyll
+git add .nojekyll
+
+# Add CNAME file
+echo "hadoopy.co" > CNAME
+
+# Update git
+mv ./doc_temp/* .
+git add *
+git commit -m "Docs Commit"
+
+# Push this copy to the true git repo (local on this machine)
+git push -f origin gh-pages
+
+# Return to the base doc directory and remove temp clone
+popd
+rm -rf temp_clone
+
+# Push the final copy to github
+git push -f origin gh-pages
