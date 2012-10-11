19c379950ccfa18bc4a945d81d9b8ddf55410b40
Brandyn A. White <bwhite@dappervision.com>
2012-10-11 14:53:32 -0400

    Use cmdenvs to set map/reduce tasks as CDH4 removed numMapTasks from streaming
    Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- hadoopy/_runner.py
+++ hadoopy/_runner.py
@@ -151,8 +151,8 @@ def launch(in_name, out_name, script_path, partitioner=False, files=(), jobconfs
     :param pipe: If true (default) then call user code through a pipe to isolate it and stop bugs when printing to stdout.  See project docs.
     :param python_cmd: The python command to use. The default is "python". Can be used to override the system default python, e.g. python_cmd = "python2.6"
 
-    :param num_mappers: The number of mappers to use, i.e. the argument given to 'numMapTasks'. If None, then do not specify this argument to hadoop streaming.
-    :param num_reducers: The number of reducers to use, i.e. the argument given to 'numReduceTasks'. If None, then do not specify this argument to hadoop streaming.
+    :param num_mappers: The number of mappers to use (i.e., cmdenv mapred.map.tasks=num_mappers).
+    :param num_reducers: The number of reducers to use (i.e., cmdenv mapred.reduce.tasks=num_reducers).
     :param script_dir: Where the script is relative to working dir, will be prefixed to script_path with a / (default '' is current dir)
     :param remove_ext: If True, remove the script extension (default False)
     :param check_script: If True, then copy script and .py(c) files to a temporary directory and verify that it can be executed.  This catches the majority of errors related to not included locally imported files. (default True)
@@ -233,10 +233,12 @@ def launch(in_name, out_name, script_path, partitioner=False, files=(), jobconfs
     if partitioner:
         cmd += ['-partitioner',
                 '"%s"' % (partitioner)]
-    if num_mappers:
-        cmd += ['-numMapTasks', "'%i'"%(int(num_mappers))]
-    if num_reducers:
-        cmd += ['-numReduceTasks', "'%i'"%(int(num_reducers))]
+    if num_mappers is not None:
+        if 'mapred.map.tasks' not in cmdenvs:
+            cmdenvs['mapred.map.tasks'] = str(num_mappers)
+    if num_reducers is not None:
+        if 'mapred.reduce.tasks' not in cmdenvs:
+            cmdenvs['mapred.reduce.tasks'] = str(num_reducers)
     # Add files
     if copy_script:
         files = list(files)
